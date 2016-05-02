15bca5af5d43f11cb75a3b2947b39447b2d36130
Juan Marcos Narro Poma <juan_marcosn@hotmail.com>
2016-05-01 21:39:16 -0500

    minireto

---
+++ pruebaGit/miniRetos.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var num = 1...100
+
+
+for i in num {
+    
+    
+    if i  % 5 == 0
+    {
+        print("\(i) BINGO")
+    }
+    
+    if i  % 2 == 0
+    {
+        print("\(i) PAR")
+    }
+    
+    if i % 2 != 0
+    {
+        print("\(i) IMPAR")
+    }
+    if i >= 30 && i <= 40
+    {
+        print("\(i) VIVA SWIFT")
+    }
+    
+    
+}
+
